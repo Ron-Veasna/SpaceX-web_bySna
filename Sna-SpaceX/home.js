a26ea92a60c0c6ce68aee86e598b2c15904f82b5
Ron Veasna <vaing133@gmail.com>
2023-11-22 20:06:23 +0700

    New Update

---
+++ Sna-SpaceX/home.js
@@ -0,0 +1,12 @@
+function sna(){
+    alert("Boy Sna Super Strongest in the world, Facebook: Ron Veasna");
+}
+function ty(){
+    alert("Bro Ty Hacker besdong srey srey Top 1 Cambodia, Facebook: Sam EangTy");
+}
+function heng(){
+    alert("Bro Heng is the handsome man  in Cambodia, Facebook: Sean KimHeng")
+}
+function sumbit(){
+    alert("Sorry you can not send to us!, this web just for testing haha")
+}
